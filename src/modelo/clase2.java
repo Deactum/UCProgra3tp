93b495a5fbbee578ca5d0ae2a5ededbf5d8a7af3
dfleitas <dfleitas@DANILOFLEITAS.hcgrupo.local>
2020-05-19 19:11:33 -0400

    se agrego una clase con un comentario

---
+++ src/modelo/clase2.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author dfleitas
+ */
+public class clase2 {
+    private int h;
+    //creado en la notebook
+}
