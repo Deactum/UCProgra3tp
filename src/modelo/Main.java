f234a8f5d676740c781bb0ec212f60dcbecd099d
Deactum <48319175+Deactum@users.noreply.github.com>
2020-05-19 16:00:26 -0400

    Update Main.java
    se agrego un comentario

--- src/modelo/Main.java
+++ src/modelo/Main.java
@@ -13,6 +13,8 @@ package modelo;
 public class Main {
     public static void main(String args[]){
         System.out.println("hola mundo");
+        //escrito desde la web
+        
 
        
     }
