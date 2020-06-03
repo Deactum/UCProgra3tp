cad2c1e03e84f9f7ce3ad668ed1761f8b58e2147
Enzo234 <65834243+Enzo234@users.noreply.github.com>
2020-06-02 23:50:02 -0300

    Aztualizar AgregarPonencia

--- src/interfaz/AgregarPonencia.java
+++ src/interfaz/AgregarPonencia.java
@@ -1,10 +1,18 @@
 package interfaz;
 
+import java.util.Date;
+
 /**
  *
  * @author Enzo
  */
 public class AgregarPonencia extends javax.swing.JDialog {
+    
+    private String titulo;
+    private Date fecha;
+    private String nomInves;
+    private String descrip;
+    private String tipoPone;
 
     public AgregarPonencia(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
@@ -12,46 +20,153 @@ public class AgregarPonencia extends javax.swing.JDialog {
         setResizable(false);
         setLocationRelativeTo(null);
     }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+    
+    public String getNomInves() {
+        return nomInves;
+    }
+    
+    public String getDescrip() {
+        return descrip;
+    }
+    
+    public String getTipoPone() {
+        return tipoPone;
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnVolver = new javax.swing.JButton();
+        btnAceptar = new javax.swing.JButton();
+        btnCancelar = new javax.swing.JButton();
+        etiTitulo = new javax.swing.JLabel();
+        etiFecha = new javax.swing.JLabel();
+        etiNomInvestigador = new javax.swing.JLabel();
+        etiDescripcion = new javax.swing.JLabel();
+        etiTipoPonencia = new javax.swing.JLabel();
+        txtTitulo = new javax.swing.JTextField();
+        jdFecha = new com.toedter.calendar.JDateChooser();
+        txtNomInvestigador = new javax.swing.JTextField();
+        txtDescripcion = new javax.swing.JTextField();
+        cboTipoPonencia = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle("Agregar una ponencia");
 
-        btnVolver.setText("Volver");
-        btnVolver.addActionListener(new java.awt.event.ActionListener() {
+        btnAceptar.setText("Aceptar");
+        btnAceptar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnVolverActionPerformed(evt);
+                btnAceptarActionPerformed(evt);
             }
         });
+
+        btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        etiTitulo.setText("Titulo");
+
+        etiFecha.setText("Fecha");
+
+        etiNomInvestigador.setText("Nombre del Investigador");
+
+        etiDescripcion.setText("Descripcíon");
+
+        etiTipoPonencia.setText("Tipo de Ponencia");
+
+        cboTipoPonencia.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Presencial", "Videoconferencia" }));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(155, 155, 155)
-                .addComponent(btnVolver)
-                .addContainerGap(179, Short.MAX_VALUE))
+                .addGap(33, 33, 33)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(etiTipoPonencia)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+                                .addComponent(btnAceptar)
+                                .addGap(69, 69, 69)
+                                .addComponent(btnCancelar))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(59, 59, 59)
+                                .addComponent(cboTipoPonencia, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(etiFecha)
+                            .addComponent(etiNomInvestigador)
+                            .addComponent(etiDescripcion)
+                            .addComponent(etiTitulo))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jdFecha, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtNomInvestigador, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtDescripcion, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE)))))
+                .addGap(15, 15, 15))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(247, Short.MAX_VALUE)
-                .addComponent(btnVolver)
-                .addGap(21, 21, 21))
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(etiTitulo)
+                    .addComponent(txtTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(etiFecha)
+                    .addComponent(jdFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(etiNomInvestigador)
+                    .addComponent(txtNomInvestigador, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(etiDescripcion)
+                    .addComponent(txtDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(etiTipoPonencia)
+                    .addComponent(cboTipoPonencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCancelar)
+                    .addComponent(btnAceptar))
+                .addGap(16, 16, 16))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
-        this.dispose();
-    }//GEN-LAST:event_btnVolverActionPerformed
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnCancelarActionPerformed
+
+    private void btnAceptarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAceptarActionPerformed
+        titulo = txtTitulo.getText();
+        fecha = jdFecha.getDate();
+        nomInves = txtNomInvestigador.getText();
+        descrip = txtDescripcion.getText();
+        tipoPone = cboTipoPonencia.getSelectedItem().toString();
+    }//GEN-LAST:event_btnAceptarActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -92,6 +207,17 @@ public class AgregarPonencia extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnVolver;
+    private javax.swing.JButton btnAceptar;
+    private javax.swing.JButton btnCancelar;
+    private javax.swing.JComboBox<String> cboTipoPonencia;
+    private javax.swing.JLabel etiDescripcion;
+    private javax.swing.JLabel etiFecha;
+    private javax.swing.JLabel etiNomInvestigador;
+    private javax.swing.JLabel etiTipoPonencia;
+    private javax.swing.JLabel etiTitulo;
+    private com.toedter.calendar.JDateChooser jdFecha;
+    private javax.swing.JTextField txtDescripcion;
+    private javax.swing.JTextField txtNomInvestigador;
+    private javax.swing.JTextField txtTitulo;
     // End of variables declaration//GEN-END:variables
 }
